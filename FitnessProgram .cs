d8d4d56bf5729e6fdb9fd0da4f82658a561c9b76
Abisham Y. <UT03304@unicomtic.com>
2024-10-11 10:20:46 +0530

    2,3 question

---
+++ FitnessProgram .cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgramManagementSystem
+{
+    internal class FitnessProgram
+    {
+        public FitnessProgram() { }
+
+        public FitnessProgram(string fitnessProgramId, string title, string duaration, string price)
+        {
+            this.FitnessProgramId = fitnessProgramId;
+            this.Title = title;
+            this.Duaration = duaration;
+            this.Price = price;
+        }
+
+        private string FitnessProgramId { get; set; }
+        private string Title { get; set; }
+        private string Duaration { get; set; }
+        private string Price { get; set; }
+
+    }
+}
